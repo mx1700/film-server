5d2e13ce356676c3830fc346888dbd1b0ea093f8
lizhaoguang <lizhaoguang@dangdang.com>
2017-02-06 15:10:18 +0800

    init

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //LOOK: http://stackoverflow.com/questions/23786359/laravel-migration-unique-key-is-too-long-even-if-specified
+        Schema::defaultStringLength(191);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
